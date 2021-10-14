88250457162179b9981adafbe0a4e8bf5a8ec67b
Wanderson Neto <neto@neto.com>
2021-10-14 17:38:16 -0300

    Create endpoint for document update

--- serverStreamDocumet/Controllers/documentController.js
+++ serverStreamDocumet/Controllers/documentController.js
@@ -26,21 +26,21 @@ module.exports = {
 
         async function (req, res) {
 
-            console.log(req.body)
+            console.log(req.body.data)
 
-            Document.findOne({ _id: req.params.id }, (err, document) => {
+             Document.findOne({ _id: req.body.data._id }, (err, document) => {
                 if (err) {
                     return res.status(404).json({
                         err,
-                        message: 'Documento nã encontrado!',
+                        message: 'Documento não encontrado!',
                     })
                 }
 
                 console.log(document)
 
-                document.data = req.body.data
-                document
-                    .save()
+                const upDocument = { $set: req.body.data}
+                Document
+                    .updateOne(document, upDocument)
                     .then(() => {
                         return res.status(200).json(document)
                     })
@@ -51,27 +51,27 @@ module.exports = {
                         })
                     })
 
-            })
+            }) 
         },
 
     new:
 
         async function (req, res) {
 
-            console.log(req.body)
+            console.log(req.body.data)
 
-            /*  Document.save(req.body)
-             .then(() => {
-                 return res.status(200).json(document)
-             })
-             .catch(error => {
-                 return res.status(404).json({
-                     error,
-                     message: 'Movie not updated!',
-                 })
-             })
+            Document
+                .create({_id: req.params.id, ...req.body.data})
+                .then(() => {
+                    return res.status(200).json({message: "Document created"})
+                })
+                .catch(error => {
+                    return res.status(404).json({
+                        error,
+                        message: 'Movie not updated!',
+                    })
+                })
 
-     }) */
         },
 
 
--- serverStreamDocumet/server/Document.js
+++ serverStreamDocumet/server/Document.js
@@ -2,7 +2,28 @@ const { Schema, model } = require("mongoose")
 
 const Document = new Schema({
   _id: String,
-  data: Object,
+  user: String,
+  tipoDocumento: String,
+  assunto: String,
+  origem: String,
+  referencia: String,
+  nomeDocumentoOriginador: String,
+  documentoOriginador: String,
+  grauUrgencia: String,
+  indicadorOrigem: String,
+  indicadorDestino: String,
+  indicadorAplicacao: String,
+  sigiloDocumento: String,
+  julgamentoFonte: String,
+  julgamentoConteudo: String,
+  anexo: String,
+  descricao: String,
+  posicional: String,
+  restritivo: String,
+  aprovadorPrimario: String,
+  assinadorPrimario: String,
+  assinadorSecundario: String,
+  document: Object
 })
 
 module.exports = model("Document", Document)
